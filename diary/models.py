f1d8f996170ab4c9450cacb934f1d886c8a21192
Havard <hhel@online.no>
2020-02-12 17:35:05 +0100

    new primary key and changes to log

--- diary/models.py
+++ diary/models.py
@@ -7,14 +7,17 @@ class Diary(models.Model):
     manage = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     goal = models.TextField(null=True, blank=True, default="")
     notes = models.TextField(null=True, blank=True, default="")
-    date = models.DateField(primary_key=True, default=datetime.now, blank=False)
+    date = models.DateField(default=datetime.now, blank=False)
     feelgood = models.CharField(null=True, max_length=30, default="Fantastic")
     productivity = models.CharField(null=True, max_length=30, default="Fantastic")
     metime = models.CharField(null=True, max_length=30, default="Fantastic")
     fftime = models.CharField(null=True, max_length=30, default="Fantastic")
+    class Meta:
+        unique_together = (("manage", "date"),)
+
 #    image = models.ImageField()
+
+
 
 #    def __str__(self):
 #       return self.task + " - " + str(self.done)
-
-
